a6bb6e709a2770b095018d33583024ef942af610
Jace Benson <jace.benson@protonmail.com>
2020-03-28 01:28:29 -0500

    this is weird, not working

--- src/x-8821-now-joke/index.js
+++ src/x-8821-now-joke/index.js
@@ -1,15 +1,64 @@
 import {createCustomElement} from '@servicenow/ui-core';
 import snabbdom from '@servicenow/ui-renderer-snabbdom';
 import styles from './styles.scss';
-
+//import {JOKE} from '../getJoke';
+var http = require("https");
+var joke = {};
+var getJoke = function(callback){
+    console.log('in getJoke');
+    var returnObj = {};
+    var options = {
+        method: "GET",
+        hostname: "wizardly-wing-66188a.netlify.com",
+        path: "/.netlify/functions/server",
+        headers: {}
+    };
+    var req = http.request(options, function (res) {
+        var chunks = [];
+    
+        res.on("data", function (chunk) {
+            chunks.push(chunk);
+        });
+    
+        res.on("end", function () {
+            var body = Buffer.concat(chunks);
+			console.log(body.toString());
+			joke = JSON.parse(body);
+			return callback(JSON.parse(body));
+			//state.joke = JSON.parse(body);
+        });
+	});
+	console.log(returnObj);
+	req.end();
+}
 const view = (state, {updateState}) => {
 	return (
-		<div>I'm working with MAV to figure out NOW CLI stuff</div>
+		<div>
+			<h3>
+			I'm working with MAV to figure out NOW CLI stuff
+			</h3>
+			{JSON.stringify(state)}
+			{joke}
+		</div>
 	);
 };
 
 createCustomElement('x-8821-now-joke', {
 	renderer: {type: snabbdom},
 	view,
-	styles
+	styles,
+	properties: {
+		name: {
+			default: {"joke": "default", "punchline":"test"}
+		},
+	},
+	//transformState: ({properties})=>(properties)
+	transformState: function(properties){
+		properties.joke = 'getJoke()';
+		getJoke(function(joke){
+			properties.jokeB = joke
+			console.log('jokea', joke)
+		});
+		return properties.jokeB
+	}
 });
