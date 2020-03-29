0e1c0a2c01a8b2ae8a1899974c8da05e9d8616b8
Jace Benson <jace.benson@protonmail.com>
2020-03-29 00:25:25 -0500

    cleaned uo index.js, tried out update state w/documented stuff, works less useful

--- src/getJoke.js
+++ src/getJoke.js
@@ -1,29 +1,28 @@
-var http = require("https");
-var getJoke = function(){
-    console.log('in getJoke');
-    var returnObj = {};
-    var options = {
-        method: "GET",
-        hostname: "wizardly-wing-66188a.netlify.com",
-        path: "/.netlify/functions/server",
-        headers: {}
-    };
-    var req = http.request(options, function (res) {
-        var chunks = [];
-    
-        res.on("data", function (chunk) {
-            chunks.push(chunk);
-        });
-    
-        res.on("end", function () {
-            var body = Buffer.concat(chunks);
-            //console.log(body.toString());
-            returnObj = JSON.parse(body);
-        });
-    });
-    req.end(); 
-    return returnObj || {"crap":"didnt work"};
-}
+var http = require('https');
+var joke = {};
+var getJoke = function (callback) {
+	console.log('in getJoke');
+	var returnObj = {};
+	var options = {
+		method: "GET",
+		hostname: "wizardly-wing-66188a.netlify.com",
+		path: "/.netlify/functions/server",
+		headers: {}
+	};
+	var req = http.request(options, function (res) {
+		var chunks = [];
 
-//export const joke = getJoke();
-export const JOKE = {"joke": "I'm funny", "punchline": "not really"};
+		res.on("data", function (chunk) {
+			chunks.push(chunk);
+		});
+
+		res.on("end", function () {
+			var body = Buffer.concat(chunks);
+			console.log(body.toString());
+			joke = JSON.parse(body);
+		});
+	});
+	console.log(returnObj);
+	req.end();
+}
+export { joke, getJoke };
--- src/x-8821-now-joke/index.js
+++ src/x-8821-now-joke/index.js
@@ -1,76 +1,36 @@
 import { createCustomElement } from '@servicenow/ui-core';
 import snabbdom, { Fragment, createRef} from '@servicenow/ui-renderer-snabbdom';
 import styles from './styles.scss';
+import {joke, getJoke} from '../getJoke';
 
-/*
-import {createHttpEffect} from '@servicenow/ui-effect-http';
-
-const USER_FETCH_SUCCESS = 'USER_FETCH_SUCCESS'
-const jokeURL = "https://wizardly-wing-66188a.netlify.com/.netlify/functions/server/"
-
-const fetchUserEffect = createHttpEffect(jokeURL, {
-     method: 'GET',
-     //headers: {},
-     //pathParams: '',
-     //dataParam: '',
-     successActionType: USER_FETCH_SUCCESS
-});
-*/
-var http = require("https");
-var joke = {};
-var getJoke = function (callback) {
-	console.log('in getJoke');
-	var returnObj = {};
-	var options = {
-		method: "GET",
-		hostname: "wizardly-wing-66188a.netlify.com",
-		path: "/.netlify/functions/server",
-		headers: {}
-	};
-	var req = http.request(options, function (res) {
-		var chunks = [];
-
-		res.on("data", function (chunk) {
-			chunks.push(chunk);
-		});
-
-		res.on("end", function () {
-			var body = Buffer.concat(chunks);
-			console.log(body.toString());
-			joke = JSON.parse(body);
-		});
-	});
-	console.log(returnObj);
-	req.end();
-}
 const view = (state, { updateState }) => {
 	const buttonRef = createRef();
+	
 	return (
 		<Fragment>
 			<div>
 				<h3>
 					I'm working with MAV to figure out NOW CLI stuff
-			</h3>
+				</h3>
 			</div>
 			<div ref={buttonRef}>
 				<button on-click={function(){
-					getJoke(updateState({
-						joke: joke
-					}));
-					//dispatchEvent('USER_FETCHED')
-					/*updateState({
-						joke: "Updated Joke",
-						punchline: "Updated Punchline"
-					})*/
-					console.log('I can do something with a ref here', buttonRef.current);
-					console.log(state);
+					getJoke(updateState(
+						joke
+						/*{//this works but only sets the "joke" not the punchline
+						path: 'joke',
+						value: joke.joke,
+						operation: 'set'
+					}*/));
 				}
 				}>Submit</button>
 			</div>
 			<div>
-				{JSON.stringify(state)}
-				{joke}
+				<pre>
+				    JSON.stringify(state):{JSON.stringify(state, '', ' ')}
+				</pre>
 			</div>
+			<div> state.joke: {state.joke} </div>
 		</Fragment>
 	);
 };
@@ -79,19 +39,8 @@ createCustomElement('x-8821-now-joke', {
 	renderer: { type: snabbdom },
 	view,
 	styles,
-	properties: {
-		name: {
-			default: { "joke": "default", "punchline": "test" }
-		},
-	},
-	//transformState: ({properties})=>(properties)
-	transformState: function (properties) {
-		return properties.jokeB
-	}/*,
-	actionHandlers: {
-        'USER_FETCHED': fetchUserEffect,
-        [USER_FETCH_SUCCESS]: ({action, updateState}) => {
-			console.log('action', action);
-		}
-    }*/
+	initialState: {
+		joke: "initial",
+		punchline: "punch"
+	}
 });
